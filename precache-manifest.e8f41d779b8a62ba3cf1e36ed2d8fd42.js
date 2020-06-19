c043b452e7564fff3b0c187ef846d1fc002770e6
Dave Collison <davecollison@BaseMac.local>
2020-06-19 13:19:07 +0100

    Updates

---
+++ precache-manifest.e8f41d779b8a62ba3cf1e36ed2d8fd42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b5ac8e4f8ca3335ce674408e0f6cff",
+    "url": "/fcc-drum-machine/index.html"
+  },
+  {
+    "revision": "7de7b7419688ec518425",
+    "url": "/fcc-drum-machine/static/css/main.c4a177d4.chunk.css"
+  },
+  {
+    "revision": "8541b1dbbb0776872f51",
+    "url": "/fcc-drum-machine/static/js/2.db5b944d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-drum-machine/static/js/2.db5b944d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de7b7419688ec518425",
+    "url": "/fcc-drum-machine/static/js/main.dd909494.chunk.js"
+  },
+  {
+    "revision": "e333e0bc7fa01190699d",
+    "url": "/fcc-drum-machine/static/js/runtime-main.cd8a1630.js"
+  }
+]);
