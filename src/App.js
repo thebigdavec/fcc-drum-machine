9764f71885f35c34990a76ba625922a763ca663e
Dave Collison <davecollison@BaseMac.local>
2020-06-19 12:27:29 +0100

    bind keys to drumpads

--- src/App.js
+++ src/App.js
@@ -6,63 +6,90 @@ import Display from './Display.js';
 
 class App extends React.Component {
 	constructor(props) {
-    super(props);
-    
-    this.state = {
-      appTitle: "Drum Machine",
-      keyList: "QWEASDZXC",
-      currentDrum: "the drum",
-      banks: [[
-          {name: 'clap', url: './samples/Clap03 Drums1DOTcom.wav'},
-          {name: 'snap', url: './samples/Snap02 Drums1DOTcom.wav'},
-          {name: 'hihat', url: './samples/CHH07 Drums1DOTcom.wav'},
-          {name: 'crash', url: './samples/Crash10 Drums1DOTcom.wav'},
-          {name: 'ride', url: './samples/Ride02 Drums1DOTcom.wav'},
-          {name: 'ridecup', url: './samples/Ride03 Drums1DOTcom.wav'},
-          {name: 'snare', url: './samples/Snare22 Drums1DOTcom.wav'},
-          {name: 'tom', url: './samples/Tom28 Drums1DOTcom.wav'},
-          {name: 'kick', url: './samples/Kick01 Drums1DOTcom.wav'},
-        ],[
-          {name: 'plop', url: ''},
-          {name: 'whoosh', url: ''},
-          {name: 'click', url: ''},
-          {name: 'open hihat', url: ''},
-          {name: 'closed hihat', url: ''},
-          {name: 'ride cup', url: ''},
-          {name: 'snare', url: ''},
-          {name: 'tom', url: ''},
-          {name: 'kick', url: ''},
-        ]
-      ],
-      bank: 0
-    }
+		super(props);
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-  
-  handleClick(e) {
-    const drumLetter = e.target.innerText;
-    const drumIndex = this.state.keyList.indexOf(drumLetter);
-    const drumName = this.state.banks[this.state.bank][drumIndex].name;
-    const sound = document.getElementById(drumLetter);
+		this.state = {
+			appTitle: 'Drum Machine',
+			keyList: 'QWEASDZXC',
+			currentDrum: 'the drum',
+			banks: [
+				[
+					{ name: 'clap', url: './samples/Clap03 Drums1DOTcom.wav' },
+					{ name: 'snap', url: './samples/Snap02 Drums1DOTcom.wav' },
+					{ name: 'hihat', url: './samples/CHH07 Drums1DOTcom.wav' },
+					{ name: 'crash', url: './samples/Crash10 Drums1DOTcom.wav' },
+					{ name: 'ride', url: './samples/Ride02 Drums1DOTcom.wav' },
+					{ name: 'ridecup', url: './samples/Ride03 Drums1DOTcom.wav' },
+					{ name: 'snare', url: './samples/Snare22 Drums1DOTcom.wav' },
+					{ name: 'tom', url: './samples/Tom28 Drums1DOTcom.wav' },
+					{ name: 'kick', url: './samples/Kick01 Drums1DOTcom.wav' }
+				],
+				[
+					{ name: 'plop', url: '' },
+					{ name: 'whoosh', url: '' },
+					{ name: 'click', url: '' },
+					{ name: 'open hihat', url: '' },
+					{ name: 'closed hihat', url: '' },
+					{ name: 'ride cup', url: '' },
+					{ name: 'snare', url: '' },
+					{ name: 'tom', url: '' },
+					{ name: 'kick', url: '' }
+				]
+			],
+			bank: 0
+		};
 
-    console.log(sound);
+		this.handleClick = this.handleClick.bind(this);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
+	}
 
-    sound.currentTime = 0;
-    sound.play();
-    this.setState({
-      currentDrum: drumName
-    })
+	handleClick(e) {
+		const drumLetter = e.target.innerText;
+		this.triggerSound(drumLetter);
+	}
 
-  }
+	componentDidMount() {
+		document.addEventListener('keydown', this.handleKeyPress);
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('keydown', this.handleKeyPress);
+	}
+
+	handleKeyPress(e) {
+		e.preventDefault();
+		const drumLetter = e.key.toUpperCase();
+
+		if (this.state.keyList.includes(drumLetter)) {
+			this.triggerSound(drumLetter);
+		}
+		console.log('no such drum');
+		return 'no such drum';
+	}
+
+	triggerSound(drumLetter) {
+		const drumIndex = this.state.keyList.indexOf(drumLetter);
+		const drumName = this.state.banks[this.state.bank][drumIndex].name;
+		const sound = document.getElementById(drumLetter);
+
+		sound.currentTime = 0;
+		sound.play();
+		this.setState({
+			currentDrum: drumName
+		});
+	}
 
 	render() {
 		return (
 			<div className="App">
 				<header id="drum-machine" className="App-header">
 					<AppTitle apptitle={this.state.appTitle} />
-          <Display currentDrum={this.state.currentDrum} />
-          <Pads clickHandler={this.handleClick} controls={this.state.keyList} drums={[this.state.banks[this.state.bank]]} />
+					<Display currentDrum={this.state.currentDrum} />
+					<Pads
+						clickHandler={this.handleClick}
+						controls={this.state.keyList}
+						drums={[ this.state.banks[this.state.bank] ]}
+					/>
 				</header>
 			</div>
 		);
